8a9eb348fbed77fb9c8b62750b4d0bbf0efccd73
Julio Avila <hellboy2015@gmail.com>
2021-03-07 02:39:52 +0000

    initial commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("");
+	let redLight = "rounded-circle bg-danger myLight";
+	if (color == "red") {
+		redLight += " selected";
+	}
+	let yellowLight = "rounded-circle bg-warning myLight";
+	if (color == "yellow") {
+		yellowLight += " selected";
+	}
+	let greenLight = "rounded-circle bg-success myLight";
+	if (color == "green") {
+		greenLight += " selected";
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="wire bg-dark"></div>
+			<div
+				className="trafficLight bg-dark rounded py-1"
+				id="myTrafficLight">
+				<div className="row justify-content-center">
+					<div
+						onClick={() => setColor("red")}
+						className={redLight}></div>
+				</div>
+				<div className="row justify-content-center">
+					<div
+						onClick={() => setColor("yellow")}
+						className={yellowLight}></div>
+				</div>
+				<div className="row justify-content-center">
+					<div
+						onClick={() => setColor("green")}
+						className={greenLight}></div>
+				</div>
+			</div>
 		</div>
 	);
 }
